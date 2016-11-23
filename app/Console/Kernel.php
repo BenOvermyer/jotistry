4a5eaa838a705634a4c3c8c1dd5f93a8fb931176
Ben Overmyer <manatrance@gmail.com>
2016-11-23 08:46:36 -0600

    Made github cache run every five minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('cache:githubissues')
-            ->everyThirtyMinutes();
+            ->everyFiveMinutes();
     }
 }
