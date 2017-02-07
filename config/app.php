7af5a236942ede8067799779307fbadb3acc0406
Ben Overmyer <manatrance@gmail.com>
2017-02-07 10:05:02 -0600

    Remove Sentry.

--- config/app.php
+++ config/app.php
@@ -96,7 +96,6 @@ return [
         'Response'  => Illuminate\Support\Facades\Response::class,
         'Route'     => Illuminate\Support\Facades\Route::class,
         'Schema'    => Illuminate\Support\Facades\Schema::class,
-        'Sentry'    => Sentry\SentryLaravel\SentryFacade::class,
         'Session'   => Illuminate\Support\Facades\Session::class,
         'Storage'   => Illuminate\Support\Facades\Storage::class,
         'URL'       => Illuminate\Support\Facades\URL::class,
